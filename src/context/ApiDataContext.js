2c79b7633c3a838649d16a00870dd6021fa73616
Eoin Carrick <22carri99@gmail.com>
2022-04-18 16:26:43 +0000

    saev

--- src/context/ApiDataContext.js
+++ src/context/ApiDataContext.js
@@ -2,4 +2,37 @@ import React, { useState, createContext, useContext } from 'react';
 
 const DataContext = createContext();
 
-const ApiDataContext = () => {};
+export const ApiDataContext = ({ children }) => {
+  const [quotes, setQuotes] = useState([]);
+  const URL = process.env.QUOTES_URL_API;
+
+  try {
+    const getQuotes = async () => {
+      const response = await fetch(URL, {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': process.env.QUOTES_HOST_API,
+          'X-RapidAPI-Key': process.env.QUOTES_API,
+        },
+      });
+
+      const data = await response.json();
+      setQuotes(data);
+    };
+  } catch (error) {
+    console.log(error);
+  }
+
+  return (
+    <DataContext.Provider
+      value={{
+        quotes,
+        getQuotes,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useDataContext = () => useContext(DataContext);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApiDataContext } from './context/ApiDataContext';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
